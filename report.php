f21d931fce1ff696819d4af07378b8d692684286
StephenFehr <94208568+StephenFehr@users.noreply.github.com>
2021-11-19 13:21:50 -0800

    Update report.php

--- report.php
+++ report.php
@@ -1,6 +1,7 @@
-<html><head>
-        <title>Database List</title>
-        <style>
+<html>
+	<head>
+		<title>Database List</title>
+			<style>
             body{background-color:bisque;}
             h1{font: 28px;}
             table, th, td {
@@ -9,10 +10,56 @@
               font: 28px;
               color: blue;
             }
-        </style>
-    </head>
-    <body>
-        <br><a href="index.html"><strong>Home</strong></a><table><tbody><tr><th>First Name</th><th>Last Name</th><th>Email</th></tr><tr><td>Isaac</td><td>Lengacher</td><td>ilengacherbergeron@gmail.com</td></tr><tr><td>Yuan</td><td>Hu</td><td>Yuanhu@gmail.com</td></tr><tr><td>Dude</td><td>Man</td><td>dude@man.com</td></tr><tr><td>Mike</td><td>Tyson</td><td>A@b.com</td></tr><tr><td>s</td><td>s</td><td>s@2</td></tr> 
-		
-    
-</tbody></table></body></html>
+     	</style>
+	</head>
+	<body>
+	<?php 		
+		session_start();
+
+$DBName = "labTest";
+$TableName = "tester";
+$col = array("name", "age", "id", "joined", "birthday");
+$numcol = count($col);
+
+//connect to server and select database
+$mysqli = mysqli_connect("localhost", "cs213user", "letmein", $DBName);
+
+//create and issue the query
+$targetname = filter_input(INPUT_POST, 'username');
+
+$sql = "SELECT * FROM " . $TableName . " WHERE name = '" . $targetname . "'";
+
+$result = mysqli_query($mysqli, $sql) or die(mysqli_error($mysqli));
+
+//get the number of rows in the result set; should be 1 if a match
+if (mysqli_num_rows($result) < 1) {
+    //echo "not enough";
+    header("Location: Index.php");
+} else {
+
+
+    echo "<html>
+        <head>
+        <link rel='stylesheet' href='Birthday.css'>
+        <title>Happy Birthday</title>
+        </head>
+        <body style='background-color:bisque'>";
+
+    echo "<h2> User Information: </h2>";
+
+    while ($row = mysqli_fetch_array($result)) {
+        echo "<table><tr>";
+        for ($i = 0; $i < count($col); $i++) {
+            echo "<th>" . $col[$i];
+        }
+        echo "</tr>";
+        for ($i = 0; $i < count($col); $i++) {
+            echo"<td>" . $row[$i];
+        }
+        echo "</tr>";
+    } //while   
+}
+
+	?>
+	</body>
+</html>
