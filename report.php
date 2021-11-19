ec1ed45672f8865c1a249c082e1ed71ac2c1206c
StephenFehr <94208568+StephenFehr@users.noreply.github.com>
2021-11-19 13:26:25 -0800

    Update report.php

--- report.php
+++ report.php
@@ -16,34 +16,28 @@
 	<?php 		
 		session_start();
 
-$DBName = "labTest";
-$TableName = "tester";
-$col = array("name", "age", "id", "joined", "birthday");
+$DBName = "coscdrop";
+$TableName = "user";
+$col = array("fname", "lname", "id", "email", "phone");
 $numcol = count($col);
 
 //connect to server and select database
 $mysqli = mysqli_connect("localhost", "cs213user", "letmein", $DBName);
 
 //create and issue the query
-$targetname = filter_input(INPUT_POST, 'username');
+$targetfname = filter_input(INPUT_POST, 'fname');
+$targetlname = filter_input(INPUT_POST, 'lname');
+$targetemail = filter_input(INPUT_POST, 'email');
 
-$sql = "SELECT * FROM " . $TableName . " WHERE name = '" . $targetname . "'";
+$sql = "SELECT * FROM " . $TableName . "'";
 
 $result = mysqli_query($mysqli, $sql) or die(mysqli_error($mysqli));
 
 //get the number of rows in the result set; should be 1 if a match
 if (mysqli_num_rows($result) < 1) {
     //echo "not enough";
-    header("Location: Index.php");
+    header("Location: index.php");
 } else {
-
-
-    echo "<html>
-        <head>
-        <link rel='stylesheet' href='Birthday.css'>
-        <title>Happy Birthday</title>
-        </head>
-        <body style='background-color:bisque'>";
 
     echo "<h2> User Information: </h2>";
 
