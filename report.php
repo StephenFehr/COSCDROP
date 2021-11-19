2fe4e8d1aabb77e75585335b5821e5557537b175
StephenFehr <94208568+StephenFehr@users.noreply.github.com>
2021-11-18 21:55:29 -0800

    Create report.php

---
+++ report.php
@@ -0,0 +1,18 @@
+<html><head>
+        <title>Database List</title>
+        <style>
+            body{background-color:bisque;}
+            h1{font: 28px;}
+            table, th, td {
+              border: 1px solid black;
+              background-color: Ivory;
+              font: 28px;
+              color: blue;
+            }
+        </style>
+    </head>
+    <body>
+        <br><a href="index.html"><strong>Home</strong></a><table><tbody><tr><th>First Name</th><th>Last Name</th><th>Email</th></tr><tr><td>Isaac</td><td>Lengacher</td><td>ilengacherbergeron@gmail.com</td></tr><tr><td>Yuan</td><td>Hu</td><td>Yuanhu@gmail.com</td></tr><tr><td>Dude</td><td>Man</td><td>dude@man.com</td></tr><tr><td>Mike</td><td>Tyson</td><td>A@b.com</td></tr><tr><td>s</td><td>s</td><td>s@2</td></tr> 
+		
+    
+</tbody></table></body></html>
