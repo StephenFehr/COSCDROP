3a6b87ca047050b0c12909b9d6f0a95686e283a3
StephenFehr <94208568+StephenFehr@users.noreply.github.com>
2021-11-19 18:29:55 -0800

    Update form.php

--- form.php
+++ form.php
@@ -14,50 +14,10 @@
         </style>
     </head>
     <body>
-		
+	HELLO!
 
 
-<?php
-$servername = "localhost";
-$username = "coscdrop";
-$password = "letmein";
-$tablename = "users";
-	    
-$targetfname = filter_input(INPUT_POST, 'fname');
-$targetlname = filter_input(INPUT_POST, 'lname');
-$targetemail = filter_input(INPUT_POST, 'email');
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
-
-$sql = "INSERT INTO $tablename (fname, lname, email)
-VALUES ($targetfname, $targetlname, $targetemail)";
-
-if (mysqli_query($conn, $sql)) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-	<table>
-		  <tr>
-			<th>First name</th>
-			<th>Last name</th>
-			<th>Email</th>
-		  </tr>
-		  <tr>
-			<td>$targetfname</td>
-			<td>$targetlname</td>
-			<td>$targetlname</td>
-		  </tr>
-		</table>
-	    
-	    
-mysqli_close($conn);
-?>
       
       
 		<a href="report.php">Print out report of database</a></br>
