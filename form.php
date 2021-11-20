048cf5db598aedf818fbddfb405d3e31b2bd6fa4
StephenFehr <94208568+StephenFehr@users.noreply.github.com>
2021-11-19 19:01:56 -0800

    Update form.php

--- form.php
+++ form.php
@@ -1,4 +1,18 @@
-	    
+<html>
+    <head>
+        <title>New User Input</title>
+        <style>
+            body{background-color:bisque;}
+            h1{font: 28px;}
+            table, th, td {
+              border: 1px solid black;
+              background-color: Ivory;
+              font: 28px;
+              color: blue;
+            }
+        </style>
+    </head>
+    <body> 	    
 <?php
 	
 $servername = "localhost";
@@ -45,24 +59,10 @@ if (mysqli_query($conn, $sql)) {
 mysqli_close($conn);
 ?>
 
-<html>
-    <head>
-        <title>New User Input</title>
-        <style>
-            body{background-color:bisque;}
-            h1{font: 28px;}
-            table, th, td {
-              border: 1px solid black;
-              background-color: Ivory;
-              font: 28px;
-              color: blue;
-            }
-        </style>
-    </head>
-    <body>  
-    </body>
-</html>
+ 
+
 
 		<a href="report.php">Print out report of database</a></br>
 		<a href="index.html"><strong>Home</strong></a>
-
+    </body>
+</html>
